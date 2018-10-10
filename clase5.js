31dbef67e325fe92183b3a52eb759e1b264576e0
Antonio Góngora <antonio.gongora1491@gmail.com>
2018-10-10 18:12:49 -0500

    objetos

--- clase5.js
+++ clase5.js
@@ -1,8 +1,42 @@
-var nombre = 'antonio'
-
-function imprimirMayusculas(){
-	nombre = nombre.toUpperCase()
-	console.log(nombre)
+//objeto
+var antonio = {
+	// Declarando atributos
+	nombre: 'antonio',
+	apellido: 'Góngora',
+	edad: 28
 }
 
-imprimirMayusculas()
+var dario = {
+	nombre: 'Dario',
+	apellido: 'Susniky',
+	edad: 29
+}
+
+//funciona (opción 1)
+// function imprimirMayusculas(persona){
+// 	console.log(persona.nombre.toUpperCase())
+
+//una nueva forma de acceder directamente al atributo (opción 2)
+// function imprimirMayusculas({ nombre }){
+
+ function imprimirMayusculas(persona){
+	 var { nombre } = persona
+	console.log(nombre.toUpperCase())
+}
+
+imprimirMayusculas(antonio)
+imprimirMayusculas(dario)
+imprimirMayusculas({ nombre: "pepito" }) //Asignamos un nuevo objeto aquí mismo
+// imprimirMayusculas()
+
+function imprimirNombreEdad(persona){
+	var nombre = persona.nombre
+	var edad = persona.edad
+	console.log(`Hola ${nombre} y tengo ${edad} años`)
+}
+
+imprimirNombreEdad(antonio)
+
+function cumpleanos(persona){
+	persona.edad += 1
+}
